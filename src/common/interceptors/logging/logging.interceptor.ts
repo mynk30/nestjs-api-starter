3a50d41b64c9f1c6fe96e432f2618565e6216cea
Mayank <mayanksoni920@gmail.com>
2026-05-11 23:50:59 +0530

    commit on 11May26

--- src/common/interceptors/logging/logging.interceptor.ts
+++ src/common/interceptors/logging/logging.interceptor.ts
@@ -3,26 +3,46 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  HttpException,
 } from '@nestjs/common';
 
-import { tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler) {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> {
     const now = Date.now();
 
     const req = context.switchToHttp().getRequest();
+
     const { method, url } = req;
 
     return next.handle().pipe(
       tap(() => {
         const res = context.switchToHttp().getResponse();
-        const { statusCode } = res;
 
         console.log(
-          `${method} ${url} ${statusCode} - ${Date.now() - now}ms`,
+          `[SUCCESS] ${method} ${url} ${res.statusCode} - ${Date.now() - now}ms`,
         );
+      }),
+
+      catchError((err) => {
+        const status =
+          err instanceof HttpException
+            ? err.getStatus()
+            : 500;
+
+        console.error(
+          `[ERROR] ${method} ${url} ${status} - ${Date.now() - now}ms`,
+        );
+
+        console.error(err.message);
+
+        return throwError(() => err);
       }),
     );
   }
